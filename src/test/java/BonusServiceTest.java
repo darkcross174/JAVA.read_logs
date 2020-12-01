f7a6bc36539bae913b11b0387ebdd2665f7e43fc
Alexey Andreev <dark-cross@rambler.ru>
2020-12-01 23:06:22 +0300

    'Logs'

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+
+            BonusService service = new BonusService();
+
+            long amount = 1000_60;
+            boolean registered = true;
+
+            service.calculate(amount, registered)
+
+}
